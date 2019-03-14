bd4c74187c38c50a843d39896ebd1d07a569637a
mr5 <895252155@qq.com>
2019-03-14 18:29:01 +0800

    Initial commit

---
+++ eurekaclient/src/main/java/com/mr5/eurekaclient/EurekaclientApplication.java
@@ -0,0 +1,28 @@
+package com.mr5.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class EurekaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaclientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home() {
+        return "hello world from port " + port;
+    }
+}
