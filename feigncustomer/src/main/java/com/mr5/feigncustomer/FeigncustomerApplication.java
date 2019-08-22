bdbd0dfb1f1962ea516a00be7b18b66d7d7941fc
mr5 <895252155@qq.com>
2019-08-22 16:06:34 +0800

    init feign

---
+++ feigncustomer/src/main/java/com/mr5/feigncustomer/FeigncustomerApplication.java
@@ -0,0 +1,13 @@
+package com.mr5.feigncustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeigncustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeigncustomerApplication.class, args);
+    }
+
+}
